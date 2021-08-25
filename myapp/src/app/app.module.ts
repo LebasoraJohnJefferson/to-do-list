14d799bd9aea9541e7aa63dda2fa82915e1bab7a
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2021-08-25 01:55:24 -0700

    angular tutorial to-do-list

---
+++ myapp/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClient, HttpClientModule} from '@angular/common/http'; 
+import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    HeaderComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AddTaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
